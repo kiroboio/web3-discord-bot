bea2691f4c3e496013b6b633c7f6fa187e81cd35
Sergey <bigcheeseh@gmail.com>
2022-03-31 15:45:04 +0300

    add production constants

--- src/server/constants.ts
+++ src/server/constants.ts
@@ -1,12 +1,13 @@
 export const BOT_NAME = "web3-kirobo-vault";
 export const VAULT_URL = 'https://vault.kirobo.me';
 export const MONGO_URL = "mongodb://localhost:27017/local";
-export const DEFAULT_PORT = 3334;
+export const DEFAULT_PORT = 3000;
 export const COLORS = { primary: "#0095FF" } as const;
 export const URL =
   process.env.NODE_ENV === "development"
     ? `http://localhost:${DEFAULT_PORT}`
-    : `http://localhost:${DEFAULT_PORT}`
+    : `https://discord1213.kirobo.me`
 export const URL_METAMASK =
-  "https://metamask.app.link/dapp/web3-discord-bot.herokuapp.com";
+  "https://metamask.app.link/dapp/discord1213.kirobo.me";
 export const IPFS_GATEWAY = 'https://ipfs.io/ipfs/';
+
